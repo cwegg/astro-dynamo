1167ee47023a93fb7d40512e80d40a05e8c156da
Chris Wegg <chriswegg+github@gmail.com>
2021-01-07 11:25:28 +0100

    Add dvc to requirements

--- setup.py
+++ setup.py
@@ -2,12 +2,23 @@
 
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(name='astro_dynamo',
       version='0.1',
-      description='Tools for making dynamical models',
+      description='Adapting N-body models using pytorch',
       author='Chris Wegg',
       author_email='chriswegg+astrodynamo@gmail.com',
-      url='https://gitlab.com/chriswegg/astro-dynamo',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/cwegg/astro-dynamo',
       packages=find_packages(),
-      install_requires=['torch', 'numpy', 'matplotlib', 'scipy', 'pandas']
+      install_requires=['torch', 'numpy', 'matplotlib', 'scipy', 'pandas'],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.6'
       )
